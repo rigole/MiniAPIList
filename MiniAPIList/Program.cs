f5282ca2216a902ebcf572ff444758210c0fe757
rigole <foplacide@gmail.com>
2023-03-13 12:03:26 +0100

    get an element  by Id on the api

--- MiniAPIList/Program.cs
+++ MiniAPIList/Program.cs
@@ -10,9 +10,29 @@ builder.Services.AddDbContext<ApiDbContext>(opt => opt.UseInMemoryDatabase("Mini
 
 var app = builder.Build();
 
-
-
 app.MapGet("/shoppinglist", async (ApiDbContext db) => await db.Groceries.ToListAsync());
+
+app.MapGet("/shoppinglist/{id}", async (int id, ApiDbContext db) =>
+{
+    var grocery = await db.Groceries.FindAsync(id); 
+    return grocery != null ? Results.Ok(grocery) : Results.NotFound();
+});
+
+
+app.MapDelete("/shoppinglist/{id}", async (int id, ApiDbContext db) =>
+{
+    var grocery = await db.Groceries.FindAsync(id);
+
+    if(grocery != null)
+    {
+        db.Groceries.Remove(grocery);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
+
 
 app.MapPost("/shoppinglist", async (Grocery grocery, ApiDbContext db) => {
 
